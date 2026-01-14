8687bc566e9c10eb0b10821b414f26d8dfd95f83
Thomas Andoerson <t11omas1983@live.co.uk>
2026-01-14 17:50:17 +0000

    Adds configurable meter name for consumer metrics
    Allows callers to specify a unique `Meter` name when adding consumer metrics. This enables more granular and identifiable metric collection, improving observability and integration with `System.Diagnostics.Metrics` via `IMeterFactory`.

--- src/Defra.TradeImports.SMB.Metrics/ServiceCollectionExtensions.cs
+++ src/Defra.TradeImports.SMB.Metrics/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Metrics;
 using Microsoft.Extensions.DependencyInjection;
 using SlimMessageBus.Host.Interceptor;
 
@@ -5,9 +6,9 @@ namespace Defra.TradeImports.SMB.Metrics;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddConsumerMetrics(this IServiceCollection services)
+    public static IServiceCollection AddConsumerMetrics(this IServiceCollection services, string meterName)
     {
-		services.AddSingleton<ConsumerMetrics>();
+		services.AddSingleton<ConsumerMetrics>(sp => new ConsumerMetrics(sp.GetRequiredService<IMeterFactory>(), meterName));
 		services.AddSingleton(typeof(IConsumerInterceptor<>), typeof(MetricsInterceptor<>));
 
 		return services;
