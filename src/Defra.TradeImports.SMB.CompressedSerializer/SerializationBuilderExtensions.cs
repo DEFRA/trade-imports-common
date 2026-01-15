3a13517a886abc8022eded76c8c9a75bf2bb380f
Thomas Andoerson <t11omas1983@live.co.uk>
2026-01-15 07:00:04 +0000

    Adds advanced message serialization support
    Introduces a new `MessageDeserializer` to handle both compressed (GZIP and Base64 encoded) and uncompressed JSON messages. This enhances message processing flexibility for SlimMessageBus.
    
    Also adds a `ToStringSerializer` to allow messages to be consumed directly as strings or converted from byte arrays, simplifying integration for plain text payloads.
    
    Includes a dedicated test project to ensure the correctness and robustness of these new serialization features.

--- src/Defra.TradeImports.SMB.CompressedSerializer/SerializationBuilderExtensions.cs
+++ src/Defra.TradeImports.SMB.CompressedSerializer/SerializationBuilderExtensions.cs
@@ -1,9 +1,9 @@
-﻿using System.Text.Json;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using SlimMessageBus.Host;
 using SlimMessageBus.Host.Serialization;
 using SlimMessageBus.Host.Serialization.SystemTextJson;
+using System.Text.Json;
 
 namespace Defra.TradeImports.SMB.CompressedSerializer;
 
@@ -30,6 +30,30 @@ public static class SerializationBuilderExtensions
 			// Add the serializer as IMessageSerializer<string>
 			services.TryAddSingleton(svp =>
 				svp.GetRequiredService<CompressedJsonMessageSerializer>() as IMessageSerializer<string>
+			);
+		});
+		return builder;
+	}
+
+	/// <summary>
+	/// Registers the <see cref="IMessageSerializer"/> with implementation as <see cref="JsonMessageSerializer"/>.
+	/// </summary>
+	/// <param name="builder"></param>
+	/// <param name="options"></param>
+	/// <returns></returns>
+	public static TBuilder AddStringSerializer<TBuilder>(
+		this TBuilder builder,
+		JsonSerializerOptions? options = null
+	)
+		where TBuilder : ISerializationBuilder
+	{
+		builder.RegisterSerializer<ToStringSerializer>(services =>
+		{
+			// Add the implementation
+			services.TryAddSingleton(svp => new ToStringSerializer());
+			// Add the serializer as IMessageSerializer<string>
+			services.TryAddSingleton(svp =>
+				svp.GetRequiredService<ToStringSerializer>() as IMessageSerializer<string>
 			);
 		});
 		return builder;
